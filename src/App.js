08a0ed933a861eabab794d7779fbee6a5858c0b3
thehamzarocks <thehamzarocks@gmail.com>
2019-08-03 21:48:32 +0530

    adding classes for tracking details and viewing them

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LogDailyDetails from './LogDailyDetails';
+import History from './History';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = {
+  days: [
+    {
+      date: '2019-05-03',
+      bodyWeight: 59,
+      exercises: [
+        {
+          exerciseName: 'squats',
+          sets: 2,
+          reps: 10,
+          weight: 20
+        },
+        {
+          exerciseName: 'curls',
+          sets: 3,
+          reps: 15,
+          weight: 25
+        }
+      ]
+    },
+    {
+      date: '2019-05-07',
+      bodyWeight: 59,
+      exercises: [
+        {
+          exerciseName: '',
+          sets: 0,
+          reps: 0,
+          weight: 0
+        },
+        {
+          exerciseName: 'curls',
+          sets: 3,
+          reps: 15,
+          weight: 30
+        }
+      ]
+    }
+  ]
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      exerciseHistory: JSON.parse(localStorage.getItem("exerciseHistory")),
+      exercises: JSON.parse(localStorage.getItem("exercises"))
+    }
+    this.handleDataChange = this.handleDataChange.bind(this);
+  }
+
+  handleDataChange(submitObject) {
+    this.setState({
+      exerciseHistory: submitObject
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <LogDailyDetails data={this.state.exerciseHistory} exercises={this.state.exercises} handleDataChange={this.handleDataChange} />
+          <History data={this.state.exerciseHistory} exercises={this.state.exercises}/>
+        </header>
+
+      </div>
+    );
+  }
 }
 
 export default App;
